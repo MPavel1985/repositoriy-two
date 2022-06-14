d44caa740a24fc6dbdbf70d8a044be4feadc752b
Pavel <mitskelevitchp@gmail.com>
2022-06-14 19:44:10 +0300

    Петриченко-2

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const numberOfFilms = prompt("Сколько фильмов вы уже просмотрели?", "");
-// console.log(numberOfFilms);
-// console.log(typeof (numberOfFilms));
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -14,7 +13,13 @@ const personalMovieDB = {
 const a = prompt("Один из последних просмотренных фильмов?", "");
 const b = prompt("На сколько оцените его?", "");
 
-
 personalMovieDB.movies[a] = b;
 
-console.log(personalMovieDB);
+if (numberOfFilms === "25") {
+    alert("Иди на хуй!");
+} else if (b === "5") {
+    alert("Иди на хуй!");
+} else {
+    console.log(personalMovieDB);
+};
+
