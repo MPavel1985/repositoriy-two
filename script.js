ce413b4b69b05b828910ad9fae531d1dd37eb6a2
Pavel <mitskelevitchp@gmail.com>
2022-06-15 22:37:12 +0300

    Калькулятор

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
-const numberOfFilms = prompt("Сколько фильмов вы уже просмотрели?", "");
+// const numberOfFilms = prompt("Сколько фильмов вы уже просмотрели?", "");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-// console.log(personalMovieDB);
+// // console.log(personalMovieDB);
 
-const a = prompt("Один из последних просмотренных фильмов?", "");
-const b = prompt("На сколько оцените его?", "");
+// const a = prompt("Один из последних просмотренных фильмов?", "");
+// const b = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[a] = b;
 
-if (numberOfFilms === "25") {
-    alert("Иди на хуй!");
-} else if (b === "5") {
-    alert("Иди на хуй!");
-} else {
-    console.log(personalMovieDB);
-};
+// if (numberOfFilms === "25") {
+//     alert("Иди на хуй!");
+// } else if (b === "5") {
+//     alert("Иди на хуй!");
+// } else {
+//     console.log(personalMovieDB);
+// };
 
+for (let i = 1; i < 10; i++) {
+    const a = +prompt("Введи число А", "");
+        if (a !== true) {
+            alert("В жопу!");
+            a = +prompt("Введи число А", "")
+        }
+    const b = +prompt("Введи число Б", "");
+    let sum;
+    sum = a + b;
+    let c = confirm(sum);
+    if (c === false) {
+        alert("В жопу!");
+    } else {
+        alert("Попробуй еще!");
+    }
+}
